ffd5fa82982bafcdea157d59b1588d9d373f6ef2
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-10-10 00:13:50 +0200

    Added Natvar, Footer components, pages and RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Create from "./pages/Create";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Post />,
+      },
+      {
+        path: "/Create",
+        element: <Create />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Natvar = () =>{
+    return(
+        <div>Natvar</div>
+    )
+}
+
+export default Natvar
